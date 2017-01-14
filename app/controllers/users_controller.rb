1f7467e16d424867542153c2a3eba02549fe79a0
CKS <ckseng86@gmail.com>
2017-01-14 17:27:57 +1100

    add create user function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,18 @@
 class UsersController < ApplicationController
+  def new
+  end
+
+  def create
+    new_user = User.new
+    new_user.email = params['email'].downcase
+    new_user.username = params['username']
+    new_user.password = params['password']
+    if new_user.save
+      sessions[:user_id] = new_user.id
+      redirect_to '/'
+    else
+      @errors = new_user.errors
+      render :new
+    end
+  end
 end
