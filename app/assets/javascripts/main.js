3d6ac1e4df18d9d77d54d82fa1a4d303ab709fd4
CKS <ckseng86@gmail.com>
2017-01-16 00:24:22 +1100

    switch to use ruby inbuilt ajax function for comments

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
 
+  // ajax call when like button is clicked
   $('.like').on('click', function(event){
     var $post = $(event.target).closest('.post');
     var postId = $post.data('post-id');
@@ -9,7 +10,8 @@ $(document).ready(function() {
       method: 'post',
       data: {
         id: postId
-      }
+      },
+      dataType: 'JSON'
     };
     function updateLikesCount(response) {
       $like.text(response.likes_count);
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,14 @@
 class CommentsController < ApplicationController
   def create
-    comment = Comment.new
-    comment.context = params[:context]
-    comment.user_id = session[:user_id]
-    comment.post_id = params[:post_id]
-    if comment.save
-      redirect_to "/posts/#{params[:post_id]}"
+    @comment = Comment.new
+    @comment.context = params[:context]
+    @comment.user_id = session[:user_id]
+    @comment.post_id = params[:post_id]
+    if @comment.save
+      respond_to do |format|
+        format.html { redirect_to "/posts/#{params[:post_id]}"}
+        format.js
+      end
     end
   end
 end
