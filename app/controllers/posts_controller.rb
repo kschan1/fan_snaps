3f161da238bbd73e0dad0fc207fcb3a7fa4a2131
CKS <ckseng86@gmail.com>
2017-01-15 15:50:31 +1100

    add update and delete for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,10 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
-
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def new
@@ -25,11 +25,25 @@ class PostsController < ApplicationController
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
 
   def update
+    @post = Post.find(params[:id])
+    @post.title = params[:title]
+    @post.description = params[:description]
+    @post.image = params[:image]
+    if @post.save
+      redirect_to "/posts/#{@post.id}"
+    else
+      @errors = @post.errors.full_messages
+      render :edit
+    end
   end
 
   def destroy
+    post = Post.find(params[:id])
+    post.destroy
+    redirect_to '/posts'
   end
 end
